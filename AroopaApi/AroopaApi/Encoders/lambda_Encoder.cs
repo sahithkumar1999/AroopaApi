e87858c06796a0189e6e160d2e803a199b6b0699
Singari Sahith Kumar <64829519+sahithkumar1999@users.noreply.github.com>
2024-08-21 15:02:54 +0200

    lambda_Encoder

---
+++ AroopaApi/AroopaApi/Encoders/lambda_Encoder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AroopaApi.Encoders
+{
+    public class lambda_Encoder
+    {
+        private string chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u007f";
+
+        public List<int> Encode(string Data)
+        {
+            // Remove duplicate characters from chars
+            string uniqueChars = new string(chars.Distinct().ToArray());
+
+            // Create dictionary mapping characters to their indices
+            Dictionary<char, int> stoi = uniqueChars.Select((ch, index) => new { ch, index }).ToDictionary(pair => pair.ch, pair => pair.index);
+
+            // Encode function that converts a string to a list of integers
+            Func<string, List<int>> encoder = s => s.Select(c => stoi[c]).ToList();
+
+            // Encode the input Data using the encoder function
+            List<int> encodedData = encoder(Data);
+
+            return encodedData;
+        }
+    }
+}
---
+++ AroopaApi/GPT/Program.cs
@@ -0,0 +1,19 @@
+﻿using AroopaApi;
+using AroopaApi.Encoders;
+using System.Text;
+
+
+namespace GPT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of lambda_Encoder
+            lambda_Encoder lambda_Encoder = new lambda_Encoder();
+            Console.WriteLine("Hello, World!");
+            List<int> encoded = lambda_Encoder.Encode("hii there");
+            Console.WriteLine(string.Join(", ", encoded));
+        }
+    }
+}
