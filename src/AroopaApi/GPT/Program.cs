45e1428c1768f4573553c65b26ecae0b02499bc4
Gadiraju Anil Kumar <kumarg.mmf@gmail.com>
2024-09-01 01:00:03 +0200

    LinearRegression

--- src/AroopaApi/GPT/Program.cs
+++ src/AroopaApi/GPT/Program.cs
@@ -3,7 +3,7 @@ using AroopaApi.Encoders;
 using System.Text;
 using GPT;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
+using AroopaApi.Models.Regression;
 
 namespace GPT
 {
@@ -12,6 +12,27 @@ namespace GPT
         
         static void Main(string[] args)
         {
+            double[,] X = new double[,]
+        {
+            {1, 1},
+            {1, 2},
+            {2, 2},
+            {2, 3}
+        };
+
+            double[] y = new double[] { 6, 6, 9, 11 };
+
+            LinearRegression lr = new LinearRegression();
+            lr.Fit(X, y);
+
+            double[] predictions = lr.Predict(X);
+            Console.WriteLine("Predicted values: " + string.Join(", ", predictions));
+
+            // Coefficients and intercept
+            Console.WriteLine("Coefficients: " + string.Join(", ", lr.coef_));
+            Console.WriteLine("Intercept: " + lr.intercept_);
+
+
             basicGPT basicGPT = new basicGPT();
             basicGPT.trainGPT();
 
